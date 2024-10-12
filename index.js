a72a45361fb93d94992e4ff6051eb496a85ed1f9
IsoDevMate <oumaoduor5827@gmail.com>
2024-10-12 05:06:16 +0300

    added node-cron

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001;
 const Arena = require('bull-arena');
 const { Queue } = require('bullmq');
 const checkDbConnection = require('./testDbConnection');
-
+const cron = require('node-cron');
 
 const arenaConfig = Arena({ 
   BullMQ:Queue,
@@ -33,6 +33,15 @@ app.use('/queues', arenaConfig);
 
 app.use(express.json());
 app.use('/auth', authRoutes)
+
+cron.schedule('*/5 * * * *', () => {
+  console.log('Running keep-alive task');
+  https.get(`https://losh-site.onrender.com/keep-alive`, (resp) => {
+    console.log('Keep-alive request sent');
+  }).on('error', (err) => {
+    console.log('Error in keep-alive request: ' + err.message);
+  });
+});
 
 const startServer = () => {
   app.listen(PORT, () => {
